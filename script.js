d5329d3ed8e49e0e8b57169099e91b1e9f59957c
Andrii Sozonik <sozonik04.av@gmail.com>
2023-06-05 03:08:28 -0700

    calculate feature

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let display_bottom = document.querySelector('#bottom_row')
 let display_operation = document.querySelector('#operation') 
 let operation_buttons = document.querySelectorAll('.operation_button')
 let number_buttons = document.querySelectorAll(".number")
+let equal_button = document.querySelector(('.equal_button'))
+
+
 
 function upgradeNumber(number) {
     if (!operation_bool) {
@@ -24,18 +27,71 @@ function upgradeNumber(number) {
     }
 }
 
+function calculate() {
+    if (operation === '+') {
+        let sum = Number(firstNumber) + Number(secondNumber)
+        sum = sum.toString()
+        firstNumber = `${sum}`
+        secondNumber = ''
+        //operation = ''
+        //display_operation.textContent = operation
+        display_bottom.textContent = secondNumber
+        display_top.textContent = firstNumber
+    }
+    else if (operation === '-') {
+        let sum = Number(firstNumber) - Number(secondNumber)
+        sum = sum.toString()
+        firstNumber = `${sum}`
+        secondNumber = ''
+        //operation = ''
+        //display_operation.textContent = operation
+        display_bottom.textContent = secondNumber
+        display_top.textContent = firstNumber
+    }
+    else if (operation === '×') {
+        let sum = Number(firstNumber) * Number(secondNumber)
+        sum = sum.toString()
+        firstNumber = `${sum}`
+        secondNumber = ''
+        //operation = ''
+        //display_operation.textContent = operation
+        display_bottom.textContent = secondNumber
+        display_top.textContent = firstNumber
+    }
+    else if (operation === '÷') {
+        let sum = Number(firstNumber) / Number(secondNumber)
+        sum = sum.toString()
+        firstNumber = `${sum}`
+        secondNumber = ''
+        //operation = ''
+        //display_operation.textContent = operation
+        display_bottom.textContent = secondNumber
+        display_top.textContent = firstNumber
+    }
+    
+}
+
 number_buttons.forEach(number => {
     number.addEventListener('click', element => {
         upgradeNumber(element.target.textContent)
     })
 })
 
-operation_buttons.forEach(operation => {
-    operation.addEventListener('click', element => {
+operation_buttons.forEach(operationer => {
+    operationer.addEventListener('click', element => {
         operation = element.target.textContent
         display_operation.textContent = operation
         operation_bool = true
-        console.log(operation_bool)
+        if (secondNumber !== '') {
+            calculate()
+        } 
+
     })
+})
+
+equal_button.addEventListener('click', () => {
+    display_operation.textContent = ''
+    operation_bool = false
+    calculate()
 })
 
